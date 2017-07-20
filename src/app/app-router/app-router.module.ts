9f88d3fc01e4355eb4408262a4b9d5e5070e4546
xuqplus <445172495@qq.com>
2017-07-20 22:11:37 +0800

    三层路由嵌套

--- src/app/app-router/app-router.module.ts
+++ src/app/app-router/app-router.module.ts
@@ -9,9 +9,16 @@ import { Comp3Component } from '../comp3/comp3.component';
 import { List1Component } from '../list1/list1.component';
 import { List2Component } from '../list2/list2.component';
 
+const third: Routes = [
+  { path: 'one', component: Comp1Component },
+  { path: 'two', component: Comp2Component },
+  { path: 'three', component: Comp3Component },
+  { path: '**', redirectTo: 'one' }
+];
+
 const childRoutes: Routes = [
   { path: 'list1', component: List1Component },
-  { path: 'list2', component: List2Component },
+  { path: 'list2', component: List2Component, children: third },
   { path: '**', redirectTo: 'list1' }
 ];
 
